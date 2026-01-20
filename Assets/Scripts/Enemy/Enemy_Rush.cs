b3e2908dfe8e0d6ed6c295add0efd349d9eee491
ghdyd0514-create <ghdyd0514@gmail.com>
2026-01-20 23:36:44 +0900

    [Chore] 돌진 몬스터 스크립트 작성

--- Assets/Scripts/Enemy/Enemy_Rush.cs
+++ Assets/Scripts/Enemy/Enemy_Rush.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy_Rush : MonoBehaviour
 {
+    Animator animator;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] private float rushPower;
+
+    bool coroutineRoop;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnDetectedPlayer() 
+    {
+        if (!coroutineRoop) // 코루틴이 중첩되지 않도록 bool타입으로 통제.
+        {
+            coroutineRoop = true;
+            StartCoroutine("RushWaitingState");
+        }
+            
+    }
+
+    public void OnLeftPlayer()
+    {
+        animator.SetBool("OnDetected", false);
+    }
+
+    IEnumerator RushWaitingState()
+    {
+        animator.SetBool("OnDetected", true);
+        rigid.linearVelocity = Vector2.zero;
+        yield return new WaitForSeconds(1.5f);
+        Rush();
+    }
+
+    void Rush()
+    {
+        animator.SetTrigger("Rush");
+        if (spriteRenderer.flipX)
+            rigid.AddForce(Vector2.left * rushPower, ForceMode2D.Impulse);
+        else 
+            rigid.AddForce(Vector2.right * rushPower, ForceMode2D.Impulse);
+
+        coroutineRoop = false;
     }
 }
